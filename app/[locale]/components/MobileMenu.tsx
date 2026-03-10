48ebb675f77e99d95ded2dcab905af8d9c5e18d5
GiovaniFRA <ferreiraalencoi1@gmail.com>
2026-03-09 21:12:48 -0300

    fix: language selector mobile

--- app/[locale]/components/MobileMenu.tsx
+++ app/[locale]/components/MobileMenu.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@radix-ui/themes"
-import { Menu, Languages} from "lucide-react"
+'use client';
+import { Menu, Languages, ChevronDown} from "lucide-react"
 import { useTranslations } from "next-intl";
 import { useRouter } from 'next/navigation'
 import { useState } from "react";
+import { DropdownMenu, Button, Theme, Flex } from '@radix-ui/themes';
 
 export default function Hamburger() {
     const router = useRouter();
@@ -23,7 +24,7 @@ export default function Hamburger() {
                     onClick={() => setOpen(false)} 
                 />
             )}
-            <nav className="flex flex-col gap-4 left-0 top-[40px] px-[10px] bg-[#f5f5f5] absolute z-3 md:hidden md:gap-70">
+            <nav className="flex flex-col gap-4 left-0 top-[40px] min-w-[200px] px-[10px] bg-[#f5f5f5] absolute z-3 md:hidden md:gap-70">
                 <Button size="2" className="cursor-pointer" onClick={()=>{setOpen(!open)}}>
                     <Menu size={24} />
                 </Button>
@@ -36,31 +37,28 @@ export default function Hamburger() {
                             <a href="#experience" className="w-full py-1 px-2 text-sm font-medium">{t('experience')}</a>
                             <a href="#contact" className="w-full py-1 px-2 text-sm font-medium">{t('contact')}</a>
                     </div>
-                    <div className="flex flex-col items-center gap-4 justify-center">
-                        <Languages size={20} color='black' />
-                        <div className="flex flex-col gap-2">
-                            <button 
-                                onClick={() => handleLanguageChange('pt-br')}
-                                className={`px-3 py-1 text-[10px] font-bold rounded border transition-all ${
-                                    t('language.language') === 'pt-br' 
-                                    ? 'bg-blue-600 text-white border-blue-600' 
-                                    : 'bg-white text-gray-600 border-gray-300 hover:border-blue-400'
-                                }`}
-                            >
-                                PT
-                            </button>
-                            <button 
-                                onClick={() => handleLanguageChange('en')}
-                                className={`px-3 py-1 text-[10px] font-bold rounded border transition-all ${
-                                    t('language.language') === 'en' 
-                                    ? 'bg-blue-600 text-white border-blue-600' 
-                                    : 'bg-white text-gray-600 border-gray-300 hover:border-blue-400'
-                                }`}
-                            >
-                                EN
-                            </button>
-                        </div>
-                    </div>
+
+                    <Flex align="center" gap="1" className='z-3'>
+                        {/* <Languages size={18} /> */}
+                        <Theme>
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Button variant="ghost" size="2" className="cursor-pointer flex items-center gap-1">
+                                        {t('language.language')} 
+                                        <ChevronDown size={12} />
+                                    </Button>
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content className='z-3 bg-[#f5f5f5]' variant="soft" color="gray">
+                                    <DropdownMenu.Item onClick={() => handleLanguageChange('pt-br')}>
+                                        {t('language.portuguese')}
+                                    </DropdownMenu.Item>
+                                    <DropdownMenu.Item onClick={() => handleLanguageChange('en')}>
+                                        {t('language.english')}
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+                        </Theme>
+                    </Flex>
                 </div>
             </nav>
         </>
